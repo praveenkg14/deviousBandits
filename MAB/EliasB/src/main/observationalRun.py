dc4291d500aa39aa669c049ca8479abaca661f4f
praveenkg14 <63747218+praveenkg14@users.noreply.github.com>
2020-06-28 15:55:34 -0700

    Create observationalRun.py

---
+++ MAB/EliasB/src/main/observationalRun.py
@@ -0,0 +1,11 @@
+##########################################################################
+#
+# Observational (Intuition-Maximizing) Bandit Player.
+# Bandit player that maximizes P(y | x); evidential decision theory.
+#
+# (c) 2014 Pedro A. Ortega <pedro.ortega@gmail.com>
+#     2015 Modified by Andrew Forney
+#
+##########################################################################
+
+# function [Action, Reward, Prob, Conds] = observationalRun(theta, T, allFactors, pObs)
